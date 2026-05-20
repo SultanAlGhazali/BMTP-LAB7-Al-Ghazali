77cc2b0526b96eba041bb307a5e7e30e61a258ea
SultanAlGhazali <hawaresa@gmail.com>
2026-05-20 14:55:15 +0200

    Added Lab 9: static library, test driver and task 9.4 complete

---
+++ Lab9/prj/TestDriver/ModulesAl-Ghazali.h
@@ -0,0 +1,12 @@
+#ifndef MODULESAL_GHAZALI_H_INCLUDED
+#define MODULESAL_GHAZALI_H_INCLUDED
+
+// ЛР 8
+double s_calculation(double x, double y, double z);
+
+// ЛР 9: Додаткові функції розгалужених та ітераційних процесів
+double compute_task_9_1(double x, double y); // Розгалуження (if-else)
+double compute_task_9_2(int n);            // Цикл (for) - сума ряду
+int compute_task_9_3(int array[], int size); // Ітерація - пошук у масиві
+
+#endif // MODULESAL_GHAZALI_H_INCLUDED
